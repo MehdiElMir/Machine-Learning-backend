67392c5dc6335177d222d6bb36fe78f89ecfad7f
kawtar24 <k.mounir@mundiapolis.ma>
2024-04-13 11:15:04 +0000

    load,save:state

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,6 +4,8 @@ from .views import delete_missing_row
 from .views import delete_selected_columns
 from .views import imputate_selected_column
 from .views import linear_regression
+from .views import save_state
+from .views import load_state
 
 urlpatterns = [
     path('process-csv/', process_csv, name='process_csv'),
@@ -11,4 +13,6 @@ urlpatterns = [
     path('delete_selected_columns/', delete_selected_columns, name='delete_selected_columns'),
     path('imputate_selected_column/', imputate_selected_column, name='imputate_selected_column'),
     path('linear_regression/', linear_regression, name='linear_regression'),
+    path('save_state/', save_state, name='save_state'),
+    path('load_state/', load_state, name='load_state'),
 ]
